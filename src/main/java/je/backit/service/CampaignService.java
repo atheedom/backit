2912abcc3c62361c80ce0430ad14c58d5d5b3070
Yann Le Tallec <yann@assylias.com>
2015-03-14 21:35:25 +0000

    use actual campaign service

--- src/main/java/je/backit/service/CampaignService.java
+++ src/main/java/je/backit/service/CampaignService.java
@@ -1,40 +1,33 @@
 package je.backit.service;
 
-import java.util.ArrayList;
 import java.util.List;
-
 import javax.enterprise.context.RequestScoped;
 import javax.inject.Inject;
-
 import je.backit.control.CampaignDAO;
-import je.backit.control.MOCKCampaignDAO;
 import je.backit.entity.Campaign;
 
 @RequestScoped
 public class CampaignService implements Service<Campaign, Integer> {
 
-//  @Inject
-//  CampaignDAO campaignDAO;
-  
-  @Inject
-  MOCKCampaignDAO campaignDAO;
+  @Inject CampaignDAO campaignDAO;
+//  @Inject MOCKCampaignDAO campaignDAO;
 
   @Override
   public List<Campaign> getAll() {
-    
-    // get all data from db and create CampaignDTO
-    List<Campaign> campaigns = campaignDAO.findAll();  
 
-    for(Campaign campaign : campaigns){    
+    // get all data from db and create CampaignDTO
+    List<Campaign> campaigns = campaignDAO.findAll();
+
+    for(Campaign campaign : campaigns){
       campaign.setNoBackers(campaignDAO.getNumberOfDonors(campaign.getId()));
-      campaign.setBacked(campaignDAO.getAmountFunded(campaign.getId()));     
-    }     
-    
+      campaign.setBacked(campaignDAO.getAmountFunded(campaign.getId()));
+    }
+
     return campaigns;
   }
 
   @Override
-  public Campaign getDetails(Integer id) {  
+  public Campaign getDetails(Integer id) {
     return campaignDAO.findById(id);
   }
 
